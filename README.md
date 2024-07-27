dca4a34b095f0dfbbe60cd25161422118e4aad10
Travis Gockel <travis@gockelhut.com>
2024-07-26 23:55:08 -0600

    fix: Hack the generated code to support public dependency checking
    This makes some `build.rs` hackery to add support for running with the
    public depedency leak detection (currently run with the command line
    `cargo +nightly build -Zpublic-dependency`). Now, generated code gets
    the following fixes before use:
    
    1. All `pub` is replaced with `pub(crate)`. The dependency checker is
       not smart enough to detect that `pub` things which are used only in
       private modules or are otherwise unreachable are not part of the
       public API. It does understand that `pub(crate)` will not escape the
       crate, though. An alternative explanation is that this is not a bug
       and it is actually I who is wrong (this seems more likely).
    
    2. Add `#![allow(unused)]` as inner macros on all modules. Because
       things are now `pub(crate)`, the unused code detector is really
       unhappy.
    
    3. Comments are stripped. I have never figured out how the Tonic Build
       `disable_comments` is supposed to work, so now they are removed via a
       textual check. This allows DocTest to run again.

--- README.md
+++ README.md
@@ -10,4 +10,8 @@ F.A.Q.
 
 ### Why not `etcd-client`?
 
+One of the major things I do not like about the `etcd-client` library is the use of `&mut self` for every API.
+This is inherited from the [Tonic Build][tonic-build] generated bindings.
 A `tonic::transport::Channel` is cheap to clone, but an `etcd_client::Client` is not.
+
+[tonic-build]: https://docs.rs/tonic-build/latest/tonic_build/
--- etcdrs/build.rs
+++ etcdrs/build.rs
@@ -1,9 +1,20 @@
 #[cfg(feature = "generate")]
 mod generate {
-    use std::{env, fs, path::PathBuf};
+    use std::{env, ffi::OsStr, fs, io::Write, path::{Path, PathBuf}};
 
     type Result<T, E = Box<dyn std::error::Error>> = std::result::Result<T, E>;
 
+    /// The `ETCD_GRPC_SOURCE` variable should be pointed at a directory that has all of the relevant repositories
+    /// checked out:
+    ///
+    /// ```sh
+    /// git clone git@github.com:etcd-io/etcd.git
+    /// git clone git@github.com:googleapis/googleapis.git
+    /// git clone git@github.com:grpc-ecosystem/grpc-gateway.git
+    /// git clone git@github.com:gogo/protobuf.git gogoproto
+    /// ```
+    ///
+    /// This might be an appropriate use for submodules.
     pub fn etcd_proto_source() -> Result<PathBuf> {
         const ETCD_SOURCE_VAR: &str = "ETCD_GRPC_SOURCE";
         println!("cargo:rerun-if-env-changed={ETCD_SOURCE_VAR}");
@@ -13,6 +24,48 @@ mod generate {
         };
 
         Ok(PathBuf::from(source))
+    }
+
+    /// Some hackery because I could not figure out the proper way to do these things with `tonic-build`. See the inside
+    /// of the function for the things that are getting replaced.
+    fn fix_file(path: &Path) -> Result<()> {
+        let source = fs::read_to_string(path)?;
+        let is_aggregate = path.file_name().unwrap() == "all.rs";
+
+        let mut output = fs::File::create(path)?;
+        for line in source.lines() {
+            // # Strip Comments
+            // There is theoretically a `disable_comments` feature on Tonic build configuration, but I can't seem to
+            // make it work. Having comments interferes with `doctest` and since none of the generated files are public,
+            // there is no value to keeping the comments.
+            if line.trim_start().starts_with("///") {
+                continue
+            }
+
+            let need_allow_unused = is_aggregate && line.starts_with("pub mod ");
+            // # `pub(crate)` instead of `pub`
+            // To work with the public dependency checker (at least as of 1.82 nightly), structures and functions which
+            // are marked `pub` are public dependencies, even if the module they are in is not public. This seems to be
+            // a bug. But to work around it for now, we just replace everything with `pub(crate)`.
+            // `cargo +nightly build -Zpublic-dependency`
+            writeln!(output, "{}", line.replace("pub ", "pub(crate) "))?;
+            if need_allow_unused {
+                // # Allowing Unused Code
+                // Tonic and Prost generate a lot of code, but we do not use all of it. The compiler gives a warning for
+                // this (since we hacked `pub(crate)` on things), so tell it that we're okay.
+                writeln!(output, "    #![allow(unused)]")?;
+            }
+        }
+        output.flush().map_err(Into::into)
+    }
+
+    fn should_save_generated_files() -> bool {
+        const SAVE_GENERATED_FILES_VAR: &str = "ETCDRS_SAVE_GENERATED_FILES";
+        println!("cargo:rerun-if-env-changed={SAVE_GENERATED_FILES_VAR}");
+        let Some(var) = env::var_os(SAVE_GENERATED_FILES_VAR) else {
+            return false
+        };
+        var.to_str().unwrap() == "1"
     }
 
     pub fn run() -> Result<()> {
@@ -30,8 +83,7 @@ mod generate {
             .build_client(true)
             .use_arc_self(true)
             .generate_default_stubs(true)
-            .disable_comments(".") // <- this doesn't work...no syntax seems to. I don't want comments. This is dumb.
-            .out_dir(out_dir)
+            .out_dir(&out_dir)
             .compile(
                 &[etc_source.join("etcd/api/etcdserverpb/rpc.proto")],
                 &[
@@ -41,6 +93,27 @@ mod generate {
                     &etc_source.join("grpc-gateway"),
                 ],
             )?;
+
+        let generated_files: Vec<_> = fs::read_dir(&out_dir)?
+            .filter_map(Result::ok)
+            .filter(|entry| entry.path().extension().and_then(OsStr::to_str) == Some("rs"))
+            .map(|entry| entry.path())
+            .collect();
+
+        for gen_file in generated_files.iter() {
+            fix_file(gen_file)?;
+        }
+
+        if should_save_generated_files() {
+            let gen_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap()).join("gen-src");
+            if gen_dir.exists() {
+                fs::remove_dir_all(&gen_dir).expect("could not delete directory");
+            }
+            fs::create_dir_all(&gen_dir).expect("could not create generated output directory");
+            for src_file in generated_files {
+                fs::copy(&src_file, gen_dir.join(src_file.file_name().unwrap())).unwrap();
+            }
+        }
 
         Ok(())
     }
